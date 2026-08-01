ce963bc44a7ee3609ee7d686bfc54b83e7eb6f27
Sylvain <lazybird@users.noreply.github.com>
2026-08-01 14:31:10 +0200

    fix(template): passer astro en mode hybrid avec adapter node

--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [tailwind()]
 });
+
